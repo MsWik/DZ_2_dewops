b6b0e4d93666c2109dbb07114e67965ffdacb0ba
unknown <24perevozki@gmail.com>
2021-02-23 10:02:58 +0300

    add python script v2

--- README.md
+++ README.md
@@ -6,3 +6,18 @@
 ## Features
 
 > Adding to three repositories
+
+
+## the result of the script
+> D:\video_a\video_analyst\venv\Scripts\python.exe D:/DZ_2/add_all.py
+> git remote add origin https://gitlab.com/MsWik/DZ_2_dewops.git
+> Branch 'main' set up to track remote branch 'main' from 'origin'.
+> To https://gitlab.com/MsWik/DZ_2_dewops.git
+>    cc689bc..1f37945  main -> main
+> git remote add origin https://gitlab.com/MsWik/DZ_2_dewops.git
+> Everything up-to-date
+> Branch 'main' set up to track remote branch 'main' from 'origin'.
+> git remote add origin https://MrPihma@bitbucket.org/MrPihma/dz_2_dewops.git
+> To https://github.com/MsWik/DZ_2_dewops.git
+>   cc689bc..1f37945  main -> main
+> Branch 'main' set up to track remote branch 'main' from 'origin'.
--- add_all.py
+++ add_all.py
@@ -1,18 +1,26 @@
 from subprocess import check_call
 import shlex
 
-git_cmd=['https://gitlab.com/MsWik/DZ_2_dewops.git','https://gitlab.com/MsWik/DZ_2_dewops.git','https://github.com/MsWik/DZ_2_dewops.git']
+
+git_cmd=['https://gitlab.com/MsWik/DZ_2_dewops.git','https://gitlab.com/MsWik/DZ_2_dewops.git','https://MrPihma@bitbucket.org/MrPihma/dz_2_dewops.git']
 def add(git_cmd):
     branch_main_cmd='git branch -M main'
     check_call(shlex.split(branch_main_cmd))
     for i in git_cmd:
-            add_remote_cmd='git remote add origin '+i
-            check_call(shlex.split(add_remote_cmd))
-            push_cmd='git push -u origin main'
-            check_call(shlex.split(push_cmd))
-            delete_remote_cmd='git remote rm origin'
-            check_call(shlex.split(delete_remote_cmd))
+           try:
+                    delete_remote_cmd = 'git remote rm origin'
+                    check_call(shlex.split(delete_remote_cmd))
+           except Exception as e:
+                    print({"status": "origin not found", "message": str(e)})
+                    pass
+
+           add_remote_cmd='git remote add origin '+i
+           print(add_remote_cmd)
+           check_call(shlex.split(add_remote_cmd))
+           push_cmd='git push -u origin main'
+           check_call(shlex.split(push_cmd))
 
 
+add(git_cmd)
 
 
